a81ff48448dd64d051c2807c5e569346fd164cdb
ztz11 <1578747475@qq.com>
2025-10-10 21:05:31 +0800

    完成教师对课程的管理以及课程学生关系管理部分的内容

--- SE_PE_AI_Manager/obj/Debug/net8.0/SE_PE_AI_Manager.AssemblyInfo.cs
+++ SE_PE_AI_Manager/obj/Debug/net8.0/SE_PE_AI_Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SE_PE_AI_Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+662c608c3f23b5d545fd354e1bc7992c5e063769")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc7ca6de53b709849b448e6727c212c878e9d8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SE_PE_AI_Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SE_PE_AI_Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
