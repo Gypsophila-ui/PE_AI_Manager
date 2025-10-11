5aa3dcafae2394c5535c472f37d950a9c245fd96
ztz11 <1578747475@qq.com>
2025-10-11 14:08:06 +0800

    完成教学内容的相关操作

--- SE_PE_AI_Manager/obj/Debug/net8.0/SE_PE_AI_Manager.AssemblyInfo.cs
+++ SE_PE_AI_Manager/obj/Debug/net8.0/SE_PE_AI_Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SE_PE_AI_Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc7ca6de53b709849b448e6727c212c878e9d8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81ff48448dd64d051c2807c5e569346fd164cdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SE_PE_AI_Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SE_PE_AI_Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
